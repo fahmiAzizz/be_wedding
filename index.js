c5d6084177d80e55514aeb8c27431efea0be6161
Fahmi Aziz <fahmiaziz031102@gmail.com>
2024-04-09 07:21:05 +0700

    menambahkan body parser

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require("express")
 const app = express()
+const bodyParser = require('body-parser')
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Parse JSON bodies (as sent by API clients)
+app.use(bodyParser.json());
+
 
 require('dotenv').config()
 
